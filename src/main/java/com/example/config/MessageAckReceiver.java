617f84dc0819c0c45231855b68c27b1d43b6d5b6
fanxin <270908634@qq.com>
2024-07-17 17:24:05 +0800

    提交

--- src/main/java/com/example/config/MessageAckReceiver.java
+++ src/main/java/com/example/config/MessageAckReceiver.java
@@ -22,10 +22,6 @@ public class MessageAckReceiver implements ChannelAwareMessageListener {
       System.out.println("111");
       System.out.println("222");
       System.out.println("333");
-      System.out.println("444");
-      System.out.println("555");
-      System.out.println("666");
-      System.out.println("222");
       channel.basicAck(deliverTag, true);
     } catch (Exception e) {
       e.printStackTrace();
